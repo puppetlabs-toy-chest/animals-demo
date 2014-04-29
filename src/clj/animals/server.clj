267e9f2d45169129ce03b0c06093d2c905edfc25
Dan Lidral-Porter <dan.lidral-porter@puppetlabs.com>
2014-04-29 12:55:01 -0700

    (CAP-3) Add a puppy

--- src/clj/animals/server.clj
+++ src/clj/animals/server.clj
@@ -8,7 +8,10 @@
   (let [rand-index (->> (rand 100)
                      int
                      (format "%03d"))
-        random-kitten (str "http://stupidstuff.org/kitten/kitten" rand-index ".jpg")]
+        random-kitten (str "http://stupidstuff.org"
+                           "/kitten/kitten" rand-index
+                           ".jpg")
+        random-puppy "http://www.randomdoggiegenerator.com/randomdoggie.php"]
     {:status 200
      :headers {"Content-Type" "text/html"}
      :body
@@ -20,6 +23,7 @@
           "<body>"
           "<div>"
           "<p><img src=\"" random-kitten "\"></p>"
+          "<p><img src=\"" random-puppy "\"></p>"
           "</div>"
           "<script src=\"js/cljs.js\"></script>"
           "</body>"
